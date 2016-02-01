54f43b796db6d1985d590b05417820f9d07e4c7d
Garrett Levine <garrlevine@gmail.com>
2016-01-31 19:49:25 -0500

    site creation

---
+++ js/menu/menuData.js
@@ -0,0 +1,203 @@
+var breakfastMenu = {
+	"Eggs Benny": {
+		price: "$15",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Eggs Florentine": {
+		price: "$12",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Eggs California": {
+		price: "$15",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Eggs Portuguese": {
+		price: "$12",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Little Breakfast": {
+		price: "$15",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Simple Breakfast": {
+		price: "$12",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Big Breakfast": {
+		price: "$15",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Huge Breakfast": {
+		price: "$14",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Pancakes": {
+		price: "$14",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Banana Pancakes": {
+		price: "$15",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Bacon Pancakes": {
+		price: "$14",
+		desc: "Sollicitudin Vehicula Tortor."
+	},
+	"Beer Pancakes": {
+		price: "$14",
+		desc: "Sollicitudin Vehicula Tortor."
+	}
+}
+
+var lunchMenu = {
+	"Beef Sandwich": {
+		price: "$13",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Chicken Sandwich": {
+		price: "$13",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Avocado Sandwich": {
+		price: "$16",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Salmon Sandwich": {
+		price: "$16",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Lobster Bisque": {
+		price: "$13",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Mint Pea Soup": {
+		price: "$13",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Coconut Chowder": {
+		price: "$16",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Tomato Pumpkin Soup": {
+		price: "$12",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Pasta Salad": {
+		price: "$13",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Prawns n' Pasta": {
+		price: "$16",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Lunch Carbonara": {
+		price: "$13",
+		desc: "Quam Fringilla Commodo."
+	},
+	"Chicken Parm": {
+		price: "$16",
+		desc: "Quam Fringilla Commodo."
+	}
+}
+
+var dinnerMenu = {
+	"Steak": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Chicken": {
+		price: "$20",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Halibut": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Soy Steak": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Burger and Fries": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Pulled Pork Sandwhich": {
+		price: "$17",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Filet Sandwich": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Spaghetti": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Lasagna": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Pork Loin": {
+		price: "$21",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Chicken Parm": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	},
+	"Nacho Dinner": {
+		price: "$18",
+		desc: "Sollicitudin Vulputate Fermentum."
+	}
+}
+
+var drinkMenu = {
+	"Cab Sauv - 5oz": {
+		price: "$13",
+		desc: "Sollicitudin Sit."
+	},
+	"Merlot - 5oz": {
+		price: "$14",
+		desc: "Sollicitudin Sit."
+	},
+	"Gamay Noir - 5oz": {
+		price: "$13",
+		desc: "Sollicitudin Sit."
+	},
+	"Malbec - 5oz": {
+		price: "$16",
+		desc: "Sollicitudin Sit."
+	},
+	"Sauv Blanc - 5oz": {
+		price: "$14",
+		desc: "Sollicitudin Sit."
+	},
+	"Pinot Gris - 5oz": {
+		price: "$13",
+		desc: "Sollicitudin Sit."
+	},
+	"Chablis - 5oz": {
+		price: "$16",
+		desc: "Sollicitudin Sit."
+	},
+	"Sancerre - 5oz": {
+		price: "$16",
+		desc: "Sollicitudin Sit."
+	},
+	"Manhattan": {
+		price: "$13",
+		desc: "Sollicitudin Sit."
+	},
+	"Negroni": {
+		price: "$16",
+		desc: "Sollicitudin Sit."
+	},
+	"Old Fashioned": {
+		price: "$13",
+		desc: "Sollicitudin Sit."
+	},
+	"Dark and Stormy": {
+		price: "$14",
+		desc: "Sollicitudin Sit."
+	}
+}
